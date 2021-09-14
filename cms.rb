e8a024991307d9abdb58b5cf2f51016b4d8e1e73
Anne Tiotuico <amtiotuico@gmail.com>
2021-09-14 15:51:03 +0000

    handling nonexistent file

--- cms.rb
+++ cms.rb
@@ -3,6 +3,12 @@ require "sinatra/reloader"
 require "tilt/erubis"
 require "sinatra/content_for"
 
+configure do
+  set :erb, :escape_html => true
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
 root = File.expand_path("..", __FILE__)
 
 get "/" do
@@ -12,8 +18,17 @@ get "/" do
   erb :index
 end
 
+def view_file(file_name)
+  return file_name if Dir.children('data').include? (file_name)
+
+  session[:error] = "#{file_name} doesn't exist"
+  redirect "/"
+end
+
 get "/:filename" do
   file_path = root + "/data/" + params[:filename]
+  file_name = params[:filename]
+  view_file(file_name)
 
   headers["Content-Type"] = "text/plain"
   File.read(file_path)
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -15,8 +15,8 @@ class CMSTest < Minitest::Test
   def test_index
     get "/"
     
-    assert_equal 200, last_response.status
-    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_equal(200, last_response.status)
+    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
     assert_includes(last_response.body, "about.txt")
     assert_includes(last_response.body, "changes.txt")
     assert_includes(last_response.body, "history.txt")
@@ -25,8 +25,23 @@ class CMSTest < Minitest::Test
   def test_history
     get "/history.txt"
     
-    assert_equal 200, last_response.status
-    assert_equal "text/plain", last_response["Content-Type"]
+    assert_equal(200, last_response.status)
+    assert_equal("text/plain", last_response["Content-Type"])
     assert_includes(last_response.body, "2014 - Ruby 2.2 released.")
+  end
+  
+  def test_for_nonexistent_file
+    get "/notafile.txt"
+  
+    assert_equal(302, last_response.status)
+    
+    get last_response["Location"]
+    
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, "notafile.txt doesn't exist")
+    
+    get "/"
+    assert_equal(200, last_response.status)
+    refute_includes(last_response.body, "notafile.txt doesn't exist")
   end
 end
