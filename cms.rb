a375c7c9ac0e162fd89fa4de4a6ce0570d5dd30a
Anne Tiotuico <amtiotuico@gmail.com>
2021-09-15 15:16:28 +0000

    edit existing files

--- cms.rb
+++ cms.rb
@@ -12,34 +12,53 @@ end
 
 root = File.expand_path("..", __FILE__)
 
-get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|
-    File.basename(path)
-  end
-  erb :index
-end
-
 def render_markdown(text)
   markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   markdown.render(text)
 end
 
-def view_file(file_name)
-  return file_name if Dir.children('data').include? (file_name)
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    render_markdown(content)
+  end
+end
 
-  session[:error] = "#{file_name} doesn't exist"
-  redirect "/"
+get "/" do
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+  erb :index, layout: :layout
 end
 
 get "/:filename" do
   file_path = root + "/data/" + params[:filename]
-  file_name = params[:filename]
-  view_file(file_name)
 
-  if File.extname(file_name) == ".md"
-    render_markdown(File.read(file_path))
+  if File.exist?(file_path)
+    load_file_content(file_path)
   else
-    headers["Content-Type"] = "text/plain"
-    File.read(file_path)
+    session[:message] = "#{params[:filename]} doesn't exist."
+    redirect "/"
   end
+end
+
+get "/:filename/edit" do
+  file_path = root + "/data/" + params[:filename]
+  @filename = params[:filename]
+  @file_contents = File.read(file_path)
+  erb :edit_file, layout: :layout
+end
+
+post "/:filename" do
+  # write to the current file and input the content from the user
+  # show session message saying filename has been updated
+  # redirect to "/" index page
+  file_path = root + "/data/" + params[:filename]
+  File.write(file_path, params[:content])
+  session[:message] = "#{params[:filename]} has been updated."
+  redirect "/"
 end
